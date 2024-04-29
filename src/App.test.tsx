ac34b79b8f01e03257d71eb7a7dcb432df7b1e02
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-04-29 11:00:29 +0100

    updated with some tests

---
+++ src/App.test.tsx
@@ -0,0 +1,26 @@
+import App from "./App";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+it("should render app", () => {
+  //arrange
+  render(<App />);
+
+  //act
+  const countButton = screen.getByRole("button");
+
+  //assert
+  expect(countButton).toBeInTheDocument();
+});
+
+it("should increase count by 1 when button pressed", async () => {
+  //arrange
+  render(<App />);
+
+  //act
+  const countButton = screen.getByRole("button");
+  expect(countButton.textContent?.includes("0"));
+  // await userEvent.click(countButton)
+
+  //assert
+});
